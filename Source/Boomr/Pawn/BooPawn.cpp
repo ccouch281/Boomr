debc05eb9c1cfbdbe774e02797214387ac6d0b65
trashman281 <trashemail2911@gmail.com>
2026-01-16 23:49:40 -0600

    pawn movement, rotation, hitscan

--- Source/Boomr/Pawn/BooPawn.cpp
+++ Source/Boomr/Pawn/BooPawn.cpp
@@ -1,7 +1,175 @@
 #include "BooPawn.h"
+#include "Components/InputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/PlayerController.h"
+#include "Kismet/GameplayStatics.h"
+#include "DrawDebugHelpers.h"
+#include "Components/CapsuleComponent.h"
+
 
 ABooPawn::ABooPawn()
 {
-    // Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-    PrimaryActorTick.bCanEverTick = true;
+    //PrimaryActorTick.bCanEverTick = true;
+
+	//capsule radius and height
+	GetCapsuleComponent()->SetCapsuleRadius(58.f);
+	GetCapsuleComponent()->SetCapsuleHalfHeight(88.f);
+
+    Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+    Camera->SetupAttachment(RootComponent);
+    Camera->SetRelativeLocation(FVector(0.f, 0.f, 64.f));
+    Camera->FieldOfView = 110.f;
+    Camera->bConstrainAspectRatio = true;
+    Camera->AspectRatio = 1.7777778f;
+	Camera->bUsePawnControlRotation = true;
+
+    bJumpButtonIsHeld = false;
+    bCrouchButtonIsHeld = false;
+    CharacterMovementProps = FCharacterMovementProperties();
+    ApplyCharacterMovementProperties(CharacterMovementProps);
+
+}
+
+void ABooPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// Default bindings for movement and actions
+	PlayerInputComponent->BindAxis("MoveForward", this, &ABooPawn::MoveForward);
+	PlayerInputComponent->BindAxis("MoveBack", this, &ABooPawn::MoveBack);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ABooPawn::MoveRight);
+	PlayerInputComponent->BindAxis("MoveLeft", this, &ABooPawn::MoveLeft);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ABooPawn::MyStartJumping);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ABooPawn::MyStopJumping);
+	PlayerInputComponent->BindAction("Crouch", IE_Pressed, this, &ABooPawn::MyStartCrouching);
+	PlayerInputComponent->BindAction("Crouch", IE_Released, this, &ABooPawn::MyStopCrouching);
+	PlayerInputComponent->BindAction("Shoot", IE_Pressed, this, &ABooPawn::Shoot);
+
+	// turn and look
+	PlayerInputComponent->BindAxis("Turn", this, &ABooPawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &ABooPawn::AddControllerPitchInput);
+
+}
+
+void ABooPawn::OnMovementModeChanged(EMovementMode PrevMovementMode, uint8 PreviousCustomMode)
+{
+	Super::OnMovementModeChanged(PrevMovementMode, PreviousCustomMode);
+	if (PrevMovementMode == MOVE_Falling && GetCharacterMovement()->MovementMode == MOVE_Walking && bJumpButtonIsHeld)
+	{
+		MyStartJumping();
+	}
+}
+
+void ABooPawn::ApplyCharacterMovementProperties(const FCharacterMovementProperties& MovementProperties)
+{
+	UCharacterMovementComponent& MoveComponent = *GetCharacterMovement();
+	MoveComponent.MaxWalkSpeed = MovementProperties.WalkingMaxSpeed;
+	MoveComponent.MaxWalkSpeedCrouched = MovementProperties.CrouchWalkingMaxSpeed;
+	MoveComponent.MaxAcceleration = MovementProperties.WalkingAcceleration;
+	MoveComponent.BrakingDecelerationWalking = MovementProperties.WalkingDeceleration;
+	MoveComponent.BrakingFrictionFactor = MovementProperties.BrakingFrictionFactor;
+	MoveComponent.SetWalkableFloorAngle(MovementProperties.WalkableFloorAngle);
+	MoveComponent.GroundFriction = MovementProperties.GroundFriction;
+	MoveComponent.bCanWalkOffLedgesWhenCrouching = MovementProperties.bCanWalkOffLedgesWhenCrouching;
+	MoveComponent.bMaintainHorizontalGroundVelocity = MovementProperties.bMaintainHorizontalGroundVelocity;
+	MoveComponent.JumpZVelocity = MovementProperties.JumpZVelocity;
+	MoveComponent.BrakingDecelerationFalling = MovementProperties.BrakingDecelerationFalling;
+	MoveComponent.AirControl = MovementProperties.AirControl;
+	MoveComponent.AirControlBoostMultiplier = MovementProperties.AirControlBoostMultiplier;
+	MoveComponent.AirControlBoostVelocityThreshold = MovementProperties.AirControlBoostVelocityThreshold;
+	MoveComponent.FallingLateralFriction = MovementProperties.FallingLateralFriction;
+	MoveComponent.GravityScale = MovementProperties.Gravity;
+	MoveComponent.NavAgentProps.bCanCrouch = MovementProperties.bCanCrouch;
+}
+
+void ABooPawn::MoveForward(float Value)
+{
+	if (Value != 0.f)
+	{
+		AddMovementInput(GetActorForwardVector(), Value);
+	}
+}
+
+void ABooPawn::MoveRight(float Value)
+{
+	if (Value != 0.f)
+	{
+		AddMovementInput(GetActorRightVector(), Value);
+	}
+}
+void ABooPawn::MoveBack(float Value)
+{
+	if (Value != 0.f)
+	{
+		AddMovementInput(GetActorForwardVector(), Value);
+	}
+}
+void ABooPawn::MoveLeft(float Value)
+{
+	if (Value != 0.f)
+	{
+		AddMovementInput(GetActorRightVector(), Value);
+	}
+}
+void ABooPawn::MyStartJumping()
+{
+	Jump();
+	bJumpButtonIsHeld = true;
+}
+
+void ABooPawn::MyStopJumping()
+{
+	StopJumping();
+	bJumpButtonIsHeld = false;
+}
+void ABooPawn::MyStartCrouching()
+{
+	Crouch();
+	bCrouchButtonIsHeld = true;
+}
+void ABooPawn::MyStopCrouching()
+{
+	UnCrouch();
+	bCrouchButtonIsHeld = false;
+}
+
+void ABooPawn::Shoot()
+{
+	// hitscan
+	auto HitscanStart = Camera->GetComponentLocation();
+	auto HitscanEnd = Camera->GetComponentLocation() + Camera->GetForwardVector() * 10000.f;
+
+	FHitResult HitResult;
+	FCollisionQueryParams TraceParams;
+	TraceParams.AddIgnoredActor(this);
+	if (GetWorld()->LineTraceSingleByChannel(
+		HitResult, 
+		HitscanStart, 
+		HitscanEnd, 
+		ECC_Visibility, 
+		TraceParams
+	))
+	{
+		if (HitResult.GetActor())
+		{
+			UGameplayStatics::ApplyDamage(
+				HitResult.GetActor(), 
+				10.f, GetController(), 
+				this, 
+				UDamageType::StaticClass()
+			);
+		}
+	}
+
+	DrawDebugLine(
+		GetWorld(), 
+		HitscanStart, 
+		HitscanEnd, 
+		FColor::Red, 
+		false, 
+		2.f
+	);
+
 }
